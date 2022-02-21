e8c6de3b0140a3b1b2e29735c4170663a4d5ec30
blossom coder <blossomcoder@gmail.com>
2022-02-21 15:35:59 +0530

    Code for spacetime project commit

---
+++ src/App.js
@@ -0,0 +1,230 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-main">
+ <>
+ <div className='header'>
+ <div className='page-width'>
+ <nav className="navbar navbar-expand-lg navbar-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">spacectime</a>
+    <div className='logo'>
+<img src='logo1.png'></img>
+    </div>
+    <div className="nav-content" id="navbarSupportedContent">
+      
+      <div className="login_btn"
+      >
+        <button className="btn">login</button>
+      </div>
+    </div>
+  </div>
+</nav>
+</div>
+</div>
+ </>
+
+ <>
+ <div className='banner-top'>
+<div className='page-width'>
+<div className="container-fluid">
+<div className='top_box'>
+<div className='top_box1'>
+<img src='Frame4.png'></img>
+<div className='top-content'>
+<h2>NFT</h2>
+<p>750,265</p>
+</div>
+</div>
+
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2>IDEP spent</h2>
+<p>6.5 <strong>mm</strong></p>
+</div>
+</div>
+
+<div className='top_box1'>
+<img src='Frame5.png'></img>
+<div className='top-content'>
+<h2>NFT acuctions</h2>
+<p>910</p>
+</div>
+</div>
+
+<div className='top_box1'>
+<img src='Frame6.png'></img>
+<div className='top-content'>
+<h2>current price</h2>
+<p>72.15 <strong>USD</strong></p>
+</div>
+</div>
+
+</div>
+  </div>
+</div>
+
+ </div>
+ 
+ </>
+
+ <>
+ <div className='main-banner'>
+<div className='page-width'>
+ <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+ width="600" 
+ height="450" 
+ style={{border:0}} 
+ allowfullscreen="" loading="lazy"></iframe>
+ </div>
+ </div>
+ 
+ 
+ </>
+
+ <>
+ <div className='grid_box'>
+<div className='page-width'>
+<div className='grid-heading'>
+<h2>explore</h2>
+
+</div>
+<div className='grid-flx'>
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+
+<div className='grid1'>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d10898553.277520332!2d-103.82424511225199!3d37.52635711222262!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2sUnited%20States!5e0!3m2!1sen!2sin!4v1645423916833!5m2!1sen!2sin" 
+></iframe>
+<h6>magazine.flavor.lapse</h6>
+<p>NYC, downtown</p>
+<div className='top_box1'>
+<img src='Frame3.png'></img>
+<div className='top-content'>
+<h2><strong>81</strong> IDEP</h2>
+</div>
+</div>
+</div>
+
+
+</div>
+</div>
+
+ </div>
+ </>
+
+
+ <>
+ 
+ <footer>
+
+<div className='page-width'>
+
+  <ul>
+<li><a href=''><img src='mail.png'></img><p>info@idep.network</p></a></li>
+<li><a href=''><img src='Doc.png'></img><p>Docs</p></a></li>
+  </ul>
+</div>
+
+
+ </footer>
+ 
+ </>
+    </div>
+  );
+}
+
+export default App;
